f20043e673a2feba9c44bc4be5e9a1ea21d3b7e5
jorgepavon <pj.pavon@gmail.com>
2022-02-22 19:12:22 -0300

    add themeProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // Dependences
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from "styled-components";
+import { useSelector} from 'react-redux';
 
 // Pages
 import Home from './Pages/home';
@@ -13,26 +15,44 @@ import OrderPage from './Pages/OrderPage';
 import OrderTablePage from './Pages/OrderTablePage';
 import Wallet from './Pages/Wallet';
 
+// Redux
+import {
+    selectSessionTheme
+} from './Redux/Selectors/selectors'
+
+
 function App() {
+    const theme = useSelector(selectSessionTheme);
+
+    const themeDark = {
+        fc: "white",
+        bg: "black"
+    };
+    const themelight = {
+        fc: "black",
+        bg: "white",
+    };
+
+    let currentTheme = theme?themelight:themeDark;
 
     return (
+        <ThemeProvider theme={currentTheme}>        
             <Routes>
-            <Route exact path="/" element={ <Home/>} />           
-            <Route exact path="/signup" element={ <SignUp/>} />           
-            <Route exact path="/signin" element={ <SignIn/>} />   
-            <Route exact path="/home" element={ <AuthHome/>} />  
-            <Route exact path="/transactions" element={ <TransactionsPage/>} />            
+                <Route exact path="/" element={ <Home/>} />           
+                <Route exact path="/signup" element={ <SignUp/>} />           
+                <Route exact path="/signin" element={ <SignIn/>} />   
+                <Route exact path="/home" element={ <AuthHome/>} />  
+                <Route exact path="/transactions" element={ <TransactionsPage/>} />            
 
-            <Route exact path="/order" element={ <OrderTablePage />} /> 
-            <Route exact path="/order/form" element={ <OrderPage />} /> 
-            <Route exact path="/order/form/:id" element={ <OrderPage />} />                                      
-            <Route exact path="/subscriptions" element={ <SubscriptionPage/>} />           
-            <Route exact path="/subscriptions/form" element={ <SubscriptionFormPage/>} />                     
-            <Route path="/subscriptions/form/:id" element={ <SubscriptionFormPage/>} />  
-            <Route exact path="/wallet" element={ <Wallet/>} />  
-
-        </Routes>
-        
+                <Route exact path="/order" element={ <OrderTablePage />} /> 
+                <Route exact path="/order/form" element={ <OrderPage />} /> 
+                <Route exact path="/order/form/:id" element={ <OrderPage />} />                                      
+                <Route exact path="/subscriptions" element={ <SubscriptionPage/>} />           
+                <Route exact path="/subscriptions/form" element={ <SubscriptionFormPage/>} />                     
+                <Route path="/subscriptions/form/:id" element={ <SubscriptionFormPage/>} />  
+                <Route exact path="/wallet" element={ <Wallet/>} />
+            </Routes>
+        </ThemeProvider>
     );
 }
 
